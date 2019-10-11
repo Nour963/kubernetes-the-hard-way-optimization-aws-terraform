f13639d78e8ea5a8026c62da208c4fc7c6496fa7
Nour963 <43506141+Nour963@users.noreply.github.com>
2019-10-11 23:35:54 +0100

    Add files via upload

--- encryption.tf
+++ encryption.tf
@@ -1,39 +1,3 @@
-
-/*data "template_file" "encrypt" {
- 
-    template = "${file("./kubeconf/encrypt.tpl")}"
-
-    vars = {
-        ENCRYPTION_KEY= "${file("./key/encryptionkey.txt")}"
-    }
-
-}
-
-resource "local_file" "encrypt" {
-  content  = "${data.template_file.encrypt.rendered}"
-  filename = "./kubeconf/encryption-config.yaml"
-}
-
-resource "null_resource" "encrypt-masters" {
-
-  count = 2
-
-
-  connection {
-    type         = "ssh"
-    user         = "ubuntu"
-    host         = "${aws_instance.k8s-MSTR[count.index].public_ip}"
-    private_key = "${file("./key/k8shardkey.pem")}"
-   
-  }
-
-
-  provisioner "file" {
-    source      = "./kubeconf/encryption-config.yaml"
-    destination = "~/encryption-config.yaml"
-  }
-}*/
-
 resource "random_string" "encryptkey" {
   length = 32
   
