cc8a5dad56c5a7c985d5a97c4c0888809f6673ae
Nour963 <43506141+Nour963@users.noreply.github.com>
2019-10-11 17:31:13 +0100

    Add files via upload

---
+++ ccm_policy.tf
@@ -0,0 +1,180 @@
+#Create an IAM role
+resource "aws_iam_role" "mstr-role" {
+  name = "mstr-ccm-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+      tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role" "wrkr-role" {
+  name = "wrkr-ccm-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+      tag-key = "tag-value"
+  }
+}
+
+#MASTER POLICY FOR CLOUD-CONTROLLER-MANAGER
+resource "aws_iam_policy" "mstr-policy" {
+  name        = "master-policy"
+  description = "for cloud contreolelr manager"
+  
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeTags",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:DescribeRouteTables",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVolumes",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifyVolume",
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CreateRoute",
+        "ec2:DeleteRoute",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteVolume",
+        "ec2:DetachVolume",
+        "ec2:RevokeSecurityGroupIngress",
+        "ec2:DescribeVpcs",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:AttachLoadBalancerToSubnets",
+        "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancerPolicy",
+        "elasticloadbalancing:CreateLoadBalancerListeners",
+        "elasticloadbalancing:ConfigureHealthCheck",
+        "elasticloadbalancing:DeleteLoadBalancer",
+        "elasticloadbalancing:DeleteLoadBalancerListeners",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "elasticloadbalancing:DescribeLoadBalancerAttributes",
+        "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:ModifyLoadBalancerAttributes",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:CreateListener",
+        "elasticloadbalancing:CreateTargetGroup",
+        "elasticloadbalancing:DeleteListener",
+        "elasticloadbalancing:DeleteTargetGroup",
+        "elasticloadbalancing:DescribeListeners",
+        "elasticloadbalancing:DescribeLoadBalancerPolicies",
+        "elasticloadbalancing:DescribeTargetGroups",
+        "elasticloadbalancing:DescribeTargetHealth",
+        "elasticloadbalancing:ModifyListener",
+        "elasticloadbalancing:ModifyTargetGroup",
+        "elasticloadbalancing:RegisterTargets",
+        "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+        "iam:CreateServiceLinkedRole",
+        "kms:DescribeKey"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+#WORKER POLICY FOR CLOUD-CONTROLLER-MANAGER
+resource "aws_iam_policy" "wrkr-policy" {
+  name        = "worker-policy"
+  description = "for cloud contreolelr manager"
+
+  
+
+  policy = <<EOF
+{
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": [
+                  "ec2:DescribeInstances",
+                  "ec2:DescribeRegions",
+                  "ecr:GetAuthorizationToken",
+                  "ecr:BatchCheckLayerAvailability",
+                  "ecr:GetDownloadUrlForLayer",
+                  "ecr:GetRepositoryPolicy",
+                  "ecr:DescribeRepositories",
+                  "ecr:ListImages",
+                  "ecr:BatchGetImage"
+              ],
+              "Resource": "*"
+          } 
+      ]
+  }
+EOF
+}
+
+resource "aws_iam_policy_attachment" "master" {
+  name       = "mstr-attachment"
+  roles      = ["${aws_iam_role.mstr-role.name}"]
+  policy_arn = "${aws_iam_policy.mstr-policy.arn}"
+}
+
+resource "aws_iam_policy_attachment" "worker" {
+  name       = "wrkr-attachment"
+  roles      = ["${aws_iam_role.wrkr-role.name}"]
+  policy_arn = "${aws_iam_policy.wrkr-policy.arn}"
+}
+
+#Create EC2 Instance Profile
+resource "aws_iam_instance_profile" "mstr-profile" {
+  name = "mstr-profile"
+  role = "${aws_iam_role.mstr-role.name}"
+}
+
+
+resource "aws_iam_instance_profile" "wrkr-profile" {
+  name = "wrkr-profile"
+  role = "${aws_iam_role.wrkr-role.name}"
+}
---
+++ encryption.tf
@@ -0,0 +1,80 @@
+
+/*data "template_file" "encrypt" {
+ 
+    template = "${file("./kubeconf/encrypt.tpl")}"
+
+    vars = {
+        ENCRYPTION_KEY= "${file("./key/encryptionkey.txt")}"
+    }
+
+}
+
+resource "local_file" "encrypt" {
+  content  = "${data.template_file.encrypt.rendered}"
+  filename = "./kubeconf/encryption-config.yaml"
+}
+
+resource "null_resource" "encrypt-masters" {
+
+  count = 2
+
+
+  connection {
+    type         = "ssh"
+    user         = "ubuntu"
+    host         = "${aws_instance.k8s-MSTR[count.index].public_ip}"
+    private_key = "${file("./key/k8shardkey.pem")}"
+   
+  }
+
+
+  provisioner "file" {
+    source      = "./kubeconf/encryption-config.yaml"
+    destination = "~/encryption-config.yaml"
+  }
+}*/
+
+resource "random_string" "encryptkey" {
+  length = 32
+  
+}
+
+resource "local_file" "encryptkey" {
+  content                = base64encode(random_string.encryptkey.result)
+  filename               = "./key/Encryption-key"
+}
+
+data "template_file" "encrypt" {
+ 
+    template = "${file("./kubeconf/encrypt.tpl")}"
+
+    vars = {
+        ENCRYPTION_KEY= local_file.encryptkey.content
+    }
+
+}
+
+resource "local_file" "encrypt" {
+  content  = "${data.template_file.encrypt.rendered}"
+  filename = "./kubeconf/encryption-config.yaml"
+}
+
+resource "null_resource" "encrypt-masters" {
+
+  count = 2
+
+
+  connection {
+    type         = "ssh"
+    user         = "ubuntu"
+    host         = "${aws_instance.k8s-MSTR[count.index].public_ip}"
+    private_key  = "${file("./key/k8shardkey.pem")}"
+   
+  }
+
+
+  provisioner "file" {
+    source      = "./kubeconf/encryption-config.yaml"
+    destination = "~/encryption-config.yaml"
+  }
+}
